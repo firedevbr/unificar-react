db1f0cfaec6f4f1ab9ccd80184907aafc3480d6f
Guilherme Mallmann <guilherme.costa@kinghost.com.br>
2020-07-26 17:00:41 -0300

    autenticando rotas

---
+++ src/context/auth.js
@@ -0,0 +1,72 @@
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import Cookies from 'js-cookie';
+import Router, { useRouter } from 'next/router';
+
+import API from '../services/api';
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({children}) => {
+
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadUserFromCookies() {
+      const token = Cookies.get('auth-jwt');
+      if (token) {
+        API.defaults.headers.Authorization = `Bearer ${token}`;
+        const { data: data } = await API.get('usuarios/eu');
+        if (data) {
+          setUser(data);
+        }
+      }
+      setLoading(false);
+    }
+    loadUserFromCookies();
+  }, []);
+
+  const login = async (cpf, senha) => {
+    const { token } = await API.post('session',  { cpf, senha });
+    if (token) {
+      console.log("GOT TOKEN!");
+      Cookies.set('auth-jwt', token);
+      API.defaults.headers.Authorization = `Bearer ${token}`;
+      const { data: data } = await API.get('usuarios/eu');
+      setUser(data);
+      console.log("Got user", user);
+    }
+  };
+
+  const logout = () => {
+    Cookies.remove('auth-jwt');
+    setUser(null);
+    window.location.pathname = '/login';
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated: !!user, user, login, loading, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
+
+};
+
+export default function useAuth() {
+    return useContext(AuthContext);
+}
+
+export function ProtectRoute(Component) {
+  return () => {
+    const { user, isAuthenticated, loading } = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+      if (!isAuthenticated && !loading) {
+        Router.push('/login');
+      }
+    }, [loading, isAuthenticated]);
+
+    return (<Component {...arguments} />)
+  }
+}
